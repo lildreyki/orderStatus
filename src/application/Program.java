292edbf594668290c84c573e52128359103067be
lildreyki <lildreyki@icloud.com>
2026-05-26 22:10:18 -0300

    att

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,4 +1,26 @@
 package application;
 
+import entities.Order;
+import entities.enums.OrderStatus;
+
+import java.util.Date;
+import java.util.Scanner;
+
 public class Program {
+    void main() {
+        Scanner sc = new Scanner(System.in);
+
+        Order order = new Order(1971, new Date(), OrderStatus.PENDING_PAYMENT);
+        IO.println(order);
+
+        String os = "undelivered";
+        while (!os.equals("DELIVERED")) {
+            IO.print("Update status: ");
+            os = sc.next();
+            order = new Order(1971, new Date(), OrderStatus.valueOf(os));
+            IO.println(order);
+        }
+
+        sc.close();
+    }
 }
